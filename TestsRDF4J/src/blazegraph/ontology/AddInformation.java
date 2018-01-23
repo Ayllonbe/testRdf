0641a4729cad8429948ab8b267e83e69677b180d
aaronayllon <aaronayllon@lexam.labri.fr>
2018-01-23 08:56:10 +0100

    23/01/18

--- TestsRDF4J/src/blazegraph/ontology/AddInformation.java
+++ TestsRDF4J/src/blazegraph/ontology/AddInformation.java
@@ -1,6 +1,9 @@
 package blazegraph.ontology;
 
 import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Hashtable;
@@ -8,6 +11,14 @@ import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 
+import org.openrdf.query.MalformedQueryException;
+import org.openrdf.query.QueryEvaluationException;
+import org.openrdf.query.QueryLanguage;
+import org.openrdf.query.TupleQuery;
+import org.openrdf.query.TupleQueryResult;
+import org.openrdf.query.Update;
+import org.openrdf.query.UpdateExecutionException;
+import org.openrdf.repository.RepositoryException;
 import org.semanticweb.elk.owlapi.ElkReasonerFactory;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.IRI;
@@ -21,13 +32,18 @@ import org.semanticweb.owlapi.reasoner.OWLReasoner;
 import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
 import org.semanticweb.owlapi.vocab.OWLRDFVocabulary;
 
+import com.bigdata.rdf.sail.remote.BigdataSailRemoteRepository;
+import com.bigdata.rdf.sail.remote.BigdataSailRemoteRepositoryConnection;
+import com.bigdata.rdf.sail.webapp.client.RemoteRepository;
+import com.bigdata.rdf.sail.webapp.client.RemoteRepositoryManager;
+
 
 public class AddInformation {
 
 	
 	
 	
-	public static void main(String[] args) throws OWLOntologyCreationException {
+	public static void main(String[] args) throws Exception {
 		IRI classIRI = null;
 
 
@@ -55,7 +71,8 @@ public class AddInformation {
 			 Set<OWLClass> onto = reasoner.getSubClasses(clazz, true).getFlattened();
 				//System.out.println("[GlobalOntology] Reasoner Charged");
 				Set<OWLClass> GOThing = new HashSet<OWLClass>();
-
+				
+				//We reli
 				for(OWLClass r:onto){
 					if(reasoner.getSubClasses(r, true).getFlattened().size()>1)
 					GOThing.add(r);
@@ -63,20 +80,33 @@ public class AddInformation {
 					
 					
 				}
+				
+			/*
+			 * We initialize several Maps. 
+			 */
 			Map<OWLClass,Integer> owl2depth = new HashMap<>();
+			Map<OWLClass,Double> owl2icnuno = new HashMap<>();
+			Map<OWLClass,Integer> subOwl2depth = new HashMap<>();
+			
 			for(OWLClass sub : GOThing) {
 
 				int flag=1;
 				int level = 0;
-				
+				int numberMaxConcept = reasoner.getSubClasses(sub, false).getFlattened().size(); //I don't add +1 because that count the BottomThing. 
 				TreeSet<OWLClass> list2 = new TreeSet<OWLClass>();
 				Set<OWLClass> list1=new HashSet<OWLClass>();
+				
+				Set<Integer> depths = new HashSet<>();
 				list1.add(sub);
 				while(flag==1) 
 				{
 					for (OWLClass pere : list1) {
 						if (!pere.isBottomEntity() ) { 
 							owl2depth.put(pere, level);
+							depths.add(level);
+							double ic = 1-(Math.log10(reasoner.getSubClasses(pere,false).getFlattened().size())/Math.log10(numberMaxConcept));
+							owl2icnuno.put(pere, ic);
+							
 							list2.addAll(reasoner.getSubClasses(pere,true).getFlattened());
 									}
 				}
@@ -94,11 +124,79 @@ public class AddInformation {
 						flag=0;
 					
 				}
-				
+				subOwl2depth.put(sub, Collections.max(depths));
 				
 			
 			}
 			
-		
+			OWLClass o = manager.getOWLDataFactory().getOWLClass(IRI.create("http://purl.obolibrary.org/obo/GO_0051179"));
+			OWLClass t = manager.getOWLDataFactory().getOWLClass(IRI.create("http://purl.obolibrary.org/obo/GO_0008150"));
+			System.out.println(0.5*owl2icnuno.get(o)+0.5*(Math.log10(owl2depth.get(o))/Math.log10(subOwl2depth.get(t))));
+			
+			String querydel = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" + 
+					"		  		PREFIX go: <http://purl.obolibrary.org/obo/>\n" + 
+					"		  		prefix owl: <http://www.w3.org/2002/07/owl#>\n" + 
+					"		  		DELETE {\n" + 
+					"		  		  GRAPH <http://blazegraph/ontology.GO/gsan/>{\n" + 
+					"		  		      ?s owl:depth ?depth;\n" + 
+					"                  			owl:IC_Nuno ?icn;\n" + 
+					"                            owl:IC_Zhou ?icz.\n" + 
+					"                  \n" + 
+					"                  \n" + 
+					"                  } } WHERE{ ?s owl:depth ?depth;"
+					+ "owl:IC_Nuno ?icn;"
+					+ "owl:IC_Zhou ?icz }";
+	
+			String queryAdd = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" + 
+					"		  		PREFIX go: <http://purl.obolibrary.org/obo/>\n" + 
+					"		  		prefix owl: <http://www.w3.org/2002/07/owl#>\n" + 
+					"		  		INSERT {\n" + 
+					"		  		  GRAPH <http://blazegraph/ontology.GO/gsan/>{\n" + 
+					"		  		      ?s owl:depth ?depth;\n" + 
+					"                  			owl:IC_Nuno ?icn;\n" + 
+					"                            owl:IC_Zhou ?icz.\n" + 
+					"                  \n" + 
+					"                  \n" + 
+					"                  } } WHERE{ }";
+			
+			String sparqlEndpoint = new URL("http", "localhost", 8889, "/bigdata").toExternalForm();
+			System.out.println(sparqlEndpoint);
+			 RemoteRepositoryManager repoManager = new RemoteRepositoryManager(sparqlEndpoint);
+	        RemoteRepository remoteRepo = repoManager.getRepositoryForNamespace("aaronbase"); // defautnamespace de blazegraph
+	        BigdataSailRemoteRepository repo = remoteRepo.getBigdataSailRemoteRepository();
+	     
+				repo.initialize();
+				BigdataSailRemoteRepositoryConnection conn = repo.getConnection();
+			
+				Update triple = conn.prepareUpdate(org.openrdf.query.QueryLanguage.SPARQL, querydel);
+				triple.execute();
+				
+			for(OWLClass sub : subOwl2depth.keySet()) {
+				
+				
+					
+						
+						
+					String query = queryAdd.replace("?s", "go:"+sub.getIRI().toString().split("/")[sub.getIRI().toString().split("/").length-1] ).
+				replace("?depth", "0" ).replace("?icn", "0").replace("?icz", "0" );
+
+				
+				
+				triple = conn.prepareUpdate(org.openrdf.query.QueryLanguage.SPARQL, query);
+				triple.execute();
+			for(OWLClass te : reasoner.getSubClasses(sub, false).getFlattened()) {
+				if (!te.isBottomEntity() ) { 
+					
+				Double icz = 0.5*owl2icnuno.get(te) + 0.5*((Math.log10(owl2depth.get(te))/Math.log10(subOwl2depth.get(sub))));
+				query = queryAdd.replace("?s", "go:"+te.getIRI().toString().split("/")[te.getIRI().toString().split("/").length-1] ).
+						replace("?depth", owl2depth.get(te).toString() ).replace("?icn", owl2icnuno.get(te).toString()).replace("?icz", icz.toString() );
+				triple = conn.prepareUpdate(org.openrdf.query.QueryLanguage.SPARQL, query);
+				triple.execute();
+				}
+			}
+			}
+			
+			conn.close();
+			repoManager.close();
 	}
 }
